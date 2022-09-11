e00a5fbaf8e7d0f435cd65af232e4024bc5ab293
zekai <zekai.kostik2469@gmail.com>
2022-09-11 17:49:51 +0300

    Youtube video downloader

---
+++ youtubemp4.py
@@ -0,0 +1,16 @@
+import pytube
+
+url= "https://player.vimeo.com/video/735668691 "
+youtube = pytube.YouTube(url)
+video = youtube.streams.first()
+video = youtube.streams.get_highest_resolution()
+video.download()
+video.download('/Downloads')
+video.title
+video.video_video_id
+video.age_restricted
+
+video.streams.all()
+stream=video.streams.all()
+for i in range:
+    print(i)
